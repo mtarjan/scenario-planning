dfc016a8d73c9a1af06affcfb3bb783db03e9443
Max Tarjan <max@SFBayBird.local>
2020-06-30 14:19:12 -0700

    load salinity data

---
+++ Code_prepare_data.R
@@ -0,0 +1,33 @@
+##M Tarjan
+##San Francisco Bay Bird Observatory
+##June 30, 2020
+##Waterbird scenario planning
+
+##LOAD REQUIRED PACAKGES
+library(RODBC) ##required to connect to Access database
+library(stringr)
+library(dplyr)
+library(tidyr) ##required for spread
+library(ggplot2)
+
+##LOAD WATERBIRD DATA
+##LOAD ENVIRONMENTAL COVARIATE DATA
+wb<-"S:/Science/Waterbird/Databases - enter data here!/Cargill Pond Surveys/USGS data from Cheryl 29Jan2018/USGS_SFBBO_pond_data_11Jun2020.accdb" ##database filepath
+
+con<-odbcConnectAccess2007(wb) ##open connection to database
+
+sqlTables(con, tableType="TABLE")$TABLE_NAME ##Get names of available tables
+
+qry<-
+  "SELECT * 
+FROM SBSPWaterQuality
+"
+
+dat<-sqlQuery(con, qry); head(dat) ##import the queried table
+
+##when finished with db, close the connection
+odbcCloseAll()
+
+fig <- ggplot(data = subset(dat, select=c("Pond", "Season", "BestSalinity")), aes(x = Pond, y = BestSalinity))
+fig <- fig + geom_boxplot()
+fig
